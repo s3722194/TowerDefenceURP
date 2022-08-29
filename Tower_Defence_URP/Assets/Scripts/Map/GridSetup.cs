60c06af8cb276060acc8361f3240a6701c718265
Casey Cook <s3722194@student.rmit.edu.au>
2022-08-29 16:20:48 +1000

    basic heart and money sprites made

--- Tower_Defence_URP/Assets/Scripts/Map/GridSetup.cs
+++ Tower_Defence_URP/Assets/Scripts/Map/GridSetup.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class GridSetup : MonoBehaviour
 {
+    [SerializeField]
+    private Vector2 SouthWestCorner;
+    [SerializeField]
+    private Vector2 NorthEastCorner;
+    private GameObject grid;
     // Start is called before the first frame update
     void Start()
     {
-        
+        float x = -SouthWestCorner.x;
+        float y = -SouthWestCorner.y;
+
+        grid = GameObject.FindGameObjectWithTag("Grid");
+
+        foreach( Transform child in grid.transform)
+        {
+            GridTile tile = child.GetComponent<GridTile>();
+            if(tile != null)
+            {
+                tile.x = (int)(tile.x - x);
+                tile.y = (int)(tile.y - y);
+            }
+        }
     }
 
     // Update is called once per frame
