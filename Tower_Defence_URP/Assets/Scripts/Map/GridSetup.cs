6113b087b8e95bc35b43bd67a965be99094d3952
Casey Cook <s3722194@student.rmit.edu.au>
2022-08-29 16:38:16 +1000

    basic gridsetup script works

--- Tower_Defence_URP/Assets/Scripts/Map/GridSetup.cs
+++ Tower_Defence_URP/Assets/Scripts/Map/GridSetup.cs
@@ -12,18 +12,27 @@ public class GridSetup : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        float x = -SouthWestCorner.x;
-        float y = -SouthWestCorner.y;
+        float x = SouthWestCorner.x;
+        float y = SouthWestCorner.y;
 
         grid = GameObject.FindGameObjectWithTag("Grid");
 
         foreach( Transform child in grid.transform)
         {
+            
             GridTile tile = child.GetComponent<GridTile>();
             if(tile != null)
             {
-                tile.x = (int)(tile.x - x);
-                tile.y = (int)(tile.y - y);
+                print(child.name);
+               
+                float newX = child.transform.position.x - x;
+                float newY = child.transform.position.y - y;
+
+                // in tranform sometimes numbers get store as 0.999999 even tho its practically 1
+                // so i need to ceil it
+                tile.x = (int) Mathf.Ceil(newX);
+                tile.y = (int) Mathf.Ceil(newY);
+                tile.nodeSize = 1;
             }
         }
     }
