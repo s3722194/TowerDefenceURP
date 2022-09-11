683fd0f32ddb60a956ae817cdc07ae9384b2ebeb
Casey Cook <s3722194@student.rmit.edu.au>
2022-09-11 22:31:04 +1000

    adjusted colliders on towers

--- Tower_Defence_URP/Assets/Scripts/Aesthetics/LoadMaterial.cs
+++ Tower_Defence_URP/Assets/Scripts/Aesthetics/LoadMaterial.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class LoadMaterial : MonoBehaviour
 {
+    public List<Material> materials = new List<Material>();
+    private SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (materials.Count != 0)
+        {
+            spriteRenderer.material = materials[Random.Range(0, materials.Count)];
+        }
     }
 
     // Update is called once per frame
