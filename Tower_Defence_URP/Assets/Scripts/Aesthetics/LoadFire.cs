6b4a9f9bbe5954a3eddbe260196b3d3b83328d67
Casey Cook <s3722194@student.rmit.edu.au>
2022-09-12 19:56:43 +1000

    basic load scripts re-added to towers

--- Tower_Defence_URP/Assets/Scripts/Aesthetics/LoadFire.cs
+++ Tower_Defence_URP/Assets/Scripts/Aesthetics/LoadFire.cs
@@ -4,9 +4,33 @@ using UnityEngine;
 
 public class LoadFire : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private List<Material> materials = new List<Material>();
+    [SerializeField] private Vector2 fireTimeY;
+    [SerializeField] private Vector2 noiseScale;
+    [SerializeField] private Vector2 ashesTimeY;
+    [SerializeField] private Vector2 ashesScale;
+    [SerializeField] private Vector2 ashesAngle;
+    [SerializeField] private Vector4 noiseOffset;
+    [SerializeField] private Vector4 ashesOffset;
+    private SpriteRenderer spriteRenderer;
+    
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if(materials.Count != 0)
+        {
+            Material mat = materials[Random.Range(0, materials.Count)];
+            spriteRenderer.material = mat;
+            spriteRenderer.material.SetFloat("_NoiseScale", Random.Range(noiseScale.x, noiseScale.y));
+            spriteRenderer.material.SetFloat("_AshesScale", Random.Range(ashesScale.x, ashesScale.y));
+            spriteRenderer.material.SetFloat("_AshesAngle", Random.Range(ashesAngle.x, ashesAngle.y));
+            spriteRenderer.material.SetVector("_FireTime", new Vector2(0, Random.Range(fireTimeY.x, fireTimeY.y)));
+            spriteRenderer.material.SetVector("_AshesTimeY", new Vector2(-0.1f,Random.Range(ashesTimeY.x, ashesTimeY.y)));
+            spriteRenderer.material.SetVector("_NoiseOffset", new Vector2(Random.Range(noiseOffset.x, noiseOffset.y),
+                Random.Range(noiseOffset.z, noiseOffset.w)));
+            spriteRenderer.material.SetVector("_AshesOffset", new Vector2(Random.Range(ashesOffset.x, ashesOffset.y),
+                Random.Range(ashesOffset.z, ashesOffset.w)));
+        }
         
     }
 
